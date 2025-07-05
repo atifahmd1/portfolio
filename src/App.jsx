2593d241e657aacbf5fb7703806881739d55fab0
Atif Ahmad <rising.atif@gmail.com>
2025-07-05 21:11:59 +0530

    hero sectionn

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Hero from "./sections/Hero";
+
+function App() {
+
+  return (
+    <>
+      <p className='text-7xl'>setting up </p>
+      <Hero />
+    </>
+  )
+}
+
+export default App
